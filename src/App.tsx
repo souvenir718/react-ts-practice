49f6920c15f16a7cf7eb7a01f3db19f3654e478a
Soobin Kim <soobin.kim@fruitsfamily.com>
2022-11-09 21:10:32 +0900

    [1109] first setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const animation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Title = styled.h1`
+  color: ${props => props.theme.textColor};
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.theme.backgroundColor};
+`;
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${animation} 1s linear infinite;
+
+  ${Emoji}:hover {
+    font-size: 96px;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>Title</Title>
+    </Wrapper>
   );
 }
 
