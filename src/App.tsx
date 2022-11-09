f1727c744f22abfc4c18bf433053b7b2c98c7da4
Soobin Kim <soobin.kim@fruitsfamily.com>
2022-11-09 21:54:43 +0900

    [1109] practice state and forms(typescript)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value)
+  }
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="red"/>
-      <Circle bgColor="tomato"/>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          value={value}
+          onChange={onChange}
+        />
+        <button>login</button>
+      </form>
     </div>
   );
 }
